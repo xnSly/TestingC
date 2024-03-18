d248ff75d2e58cbd0a7672d470bc1ec826652db2
Stephen <93400341+xnSly@users.noreply.github.com>
2024-03-18 13:24:24 -0700

    Update main.c
    malloc and struct stuff pointers

--- main.c
+++ main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct Player
+struct myStruct
 {
-    char name[12];
-    int score;
+    int value;
 };
 
 int main()
 {
-    // struct = collection of related numbers ("variables")
-    // they can be of different data types
-    // listed under one name in a block of memory
-    // VERY SIMILAR to classes in other languages (but no methods)   
+    struct myStruct *Yes;
 
-    struct Player player1 = {"Sly", 5};
+    Yes = malloc(sizeof(struct myStruct));
 
-    printf("%s\n", player1.name);
-    printf("%d\n", player1.score);
+    Yes->value = 20;
+
+    printf("%d", Yes->value);
 
     return 0;
 }
