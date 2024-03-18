49726a791b652faa91e09f63cd998ff03f49732c
Stephen <93400341+xnSly@users.noreply.github.com>
2024-03-18 13:06:25 -0700

    Update main.c
    Structs hahahahahahaha

--- main.c
+++ main.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 
+struct Player
+{
+    char name[12];
+    int score;
+};
+
 int main()
 {
-    /* Variables
+    // struct = collection of related numbers ("variables")
+    // they can be of different data types
+    // listed under one name in a block of memory
+    // VERY SIMILAR to classes in other languages (but no methods)   
 
-    Variables are allocated space in memory to store a value.
-    We refer to a variable's name to access the stored value.
-    That variable now behaves as if it was the value it contains.
-    BUT we need to declare what type of data we are storing.
-    
-    */
+    struct Player player1 = {"Sly", 5};
 
-    int x; // Declaration
-    x = 123; // Initialization
-
-    int y = 321; // Declaration + Initialization
-
-    int age = 20; // Integer
-    float gpa = 3.85; // Floating point number
-    char grade = 'B'; // Single character
-    char name[] = "Sly"; // Array of characters
-
-    printf("Hello %s!\n", name);
-    printf("You are %d years old.\n", age);
-    printf("Your average grade is %c.\n", grade);
-    printf("Your gpa is %f", gpa);
+    printf("%s\n", player1.name);
+    printf("%d\n", player1.score);
 
     return 0;
 }
