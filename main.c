a5c2f8164b1ae99938fbc0c848a780f4545a8afc
Stephen <93400341+xnSly@users.noreply.github.com>
2024-03-17 20:29:42 -0700

    Update main.c
    Practicing escape sequences!

--- main.c
+++ main.c
@@ -2,7 +2,28 @@
 
 int main()
 {
-    printf("I like pizza.\n");
+    // This is a comment bing bong
+
+    /*
+        Multi-line comments are pretty cool!
+        I can write as many comments below this as I want-
+        as long as I stay within the multi-line comment :D
+
+        ESCAPE SEQUENCES:
+        - character combination consisting of a backslash \
+        followed by a letter or combination of digits.
+
+        They specify actions within a line or string of text
+        \n = newline
+        \t = tab
+    */
+
+    printf("I\nlike\npizza.\n");
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+
+    printf("\"I like ranch!\" - Abraham Lincoln probably\n");
+
     printf("It's really good!\n");
 
     return 0;
