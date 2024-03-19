2ebd30d43a0000419774ace85005a1ae347ae1ef
Stephen <93400341+xnSly@users.noreply.github.com>
2024-03-18 20:48:43 -0700

    Update main.c
    Simple calculate area function

--- main.c
+++ main.c
@@ -1,43 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct Stack
-{
-    char *stackptr;
-
-    unsigned short capacity;
-    unsigned short occupied;
-    unsigned short element_size;
-
-} Stack;
-
-void allocStack(Stack *tStack)
-{
-    tStack->stackptr = ((int*) malloc(tStack->capacity));
+int calculateRectangleArea(int width, int height) {
+    return width * height;
 }
 
-void pushStack(Stack *tStack, void *tValue)
-{
-    memcpy(tStack->stackptr, tValue, sizeof(tValue));
-
-    tStack->stackptr[tStack->occupied] = tValue;
-
-    tStack->occupied++;
-}
-
-int popStack(Stack *tStack)
-{
-    tStack->occupied--;
-
-    return tStack->stackptr[tStack->occupied];
-}
-
-int main()
-{
-    Stack myStack = {0, 100, 0, 0};
-
-    allocStack(&myStack);
+int main() {
+    printf("%d", calculateRectangleArea(10, 5));
 
     return 0;
 }
