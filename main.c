51f4a36113e3d78b67a627b5e3feca023c0c922f
Stephen <93400341+xnSly@users.noreply.github.com>
2024-03-20 15:57:52 -0700

    Update main.c
    basic calculator

--- main.c
+++ main.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 
-int calculateRectangleArea(int width, int height) {
-    return width * height;
-}
-
 int main() {
-    printf("%d", calculateRectangleArea(10, 5));
+    printf("Type either +, -, /, or *\n");
+
+    char operator;
+
+    scanf("%c", &operator);
+
+    printf("Type two whole numbers separated by a space to perform the calculation on.\n");
+
+    long num1;
+    long num2;
+
+    scanf("%d %d", &num1, &num2);
+
+    long result;
+    
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        default:
+            printf("The operator you provided is invalid");
+            break;
+    }
+
+    printf("%d", result);
 
     return 0;
 }
