17707db19f55d0096a8c78a42dba265373ecdb70
Stephen <93400341+xnSly@users.noreply.github.com>
2024-03-22 14:57:58 -0700

    Update main.c
    made calculator loop!!! also found out about clearing input buffer

--- main.c
+++ main.c
@@ -1,40 +1,60 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Type either +, -, /, or *\n");
+void clearInputBuffer(void) {
+    int c;
 
-    char operator;
+    while ((c = getchar()) != '\n' && c != EOF) { }
+}
 
-    scanf("%c", &operator);
+double calculateEquation(char operator[5], double num1, double num2) {
+    double result = 0.0;
 
-    printf("Type two whole numbers separated by a space to perform the calculation on.\n");
-
-    long num1;
-    long num2;
-
-    scanf("%d %d", &num1, &num2);
-
-    long result;
-    
-    switch (operator) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '/':
-            result = num1 / num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        default:
-            printf("The operator you provided is invalid");
-            break;
+    if (strcmp(operator, "+") == 0) {
+        result = num1 + num2;
+    } else if (strcmp(operator, "-") == 0) {
+        result = num1 - num2;
+    } else if (strcmp(operator, "*") == 0) {
+        result = num1 * num2;
+    } else if (strcmp(operator, "/") == 0) {
+        result = num1 / num2;
     }
 
-    printf("%d", result);
+    return result;
+}
+
+int main(void) {
+    while (1) {
+        printf("Type one of the following operators: + - / *\n");
+
+        char operator[5];
+
+        if (fgets(operator, sizeof(operator), stdin) == NULL) {
+            break;
+        }
+
+        operator[strcspn(operator, "\n")] = 0;
+
+        if (strlen(operator) == 1) {
+            printf("Type two whole numbers separated by a space to perform the calculation on.\n");
+
+            double num1;
+            double num2;
+
+            scanf("%lf %lf", &num1, &num2);
+
+            clearInputBuffer();
+
+            double result = calculateEquation(operator, num1, num2);
+
+            printf("Calculation: %lf %s %lf\n", num1, operator, num2);
+            printf("Result: %lf\n", result);
+        } else {
+            printf("Invalid operator, please try again.");
+        }
+    }
+
+    printf("Thank you for using my C calculator!");
 
     return 0;
 }
